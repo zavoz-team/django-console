e83e994bb1f2dd8b30b7c7db2424210fd054ba8d
moonshy <vladbobkb10@gmail.com>
2026-03-27 16:07:29 +0300

    [refactor] use demo UserRepository locally and prepare interfaces for external calls

---
+++ usecase/dto.py
@@ -0,0 +1,7 @@
+from typing import Any, TypeAlias
+
+# TODO: согласовать структуру DTO с Искандером
+ProfileDTO: TypeAlias = dict[str, Any]
+SegmentDTO: TypeAlias = dict[str, Any]
+JobDTO: TypeAlias = dict[str, Any]
+SystemStatusDTO: TypeAlias = dict[str, Any]
